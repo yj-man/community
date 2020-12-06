9d8078e51e5d14bcbbc22f6f6ad7939fb7222cc0
yangjie <1308602407@qq.com>
2020-12-06 16:29:50 +0800

    init repo

---
+++ src/test/java/life/majiang/community/CommunityApplicationTests.java
@@ -0,0 +1,15 @@
+package life.majiang.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
